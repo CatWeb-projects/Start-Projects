92af904f1591866025f691412e779d946db84474
Theodore Uzun <53522791+Theo-projects@users.noreply.github.com>
2020-06-23 10:12:26 +0300

    commitone

---
+++ src/components/Check.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Check extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+this.hadleClickThis = (e) => {
+  [e.target.name] = e.target.value;
+};
+
+export default Check;
