de4e3f9d5c044c2ddffe5762ef016d6a83f98ee7
CatWeb <uzuntudor@yahoo.com>
2023-01-04 10:50:40 +0200

    upload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles.scss';
 import { Button, Input } from './ui/atoms';
+import { Upload } from './ui/organims/Upload/Upload';
 
 export const App = () => {
   return (
     <div className="App">
       <Button>Test</Button>
       <Input />
+      <Upload />
     </div>
   );
 };
