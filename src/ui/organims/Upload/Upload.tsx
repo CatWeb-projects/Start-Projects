7baf2e31d0c76321e8b9f5dc185a415bb65ca6cd
CatWeb <uzuntudor@yahoo.com>
2023-02-08 15:10:44 +0200

    drag and drop

--- src/ui/organims/Upload/Upload.tsx
+++ src/ui/organims/Upload/Upload.tsx
@@ -11,6 +11,20 @@ export const Upload = () => {
     return new FileReader();
   }, []);
 
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    const target = e.dataTransfer as DataTransfer;
+    const file: File = (target.files as FileList)[0];
+    if (file) {
+      setSelectedFile(file);
+      fileReader.readAsDataURL(file);
+    }
+  };
+
   const handleFileSelect = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
     const file: File = (target.files as FileList)[0];
@@ -35,7 +49,12 @@ export const Upload = () => {
       <h6>Upload</h6>
       <div className="upload-wrapper">
         {!fileUrl ? (
-          <div className="upload-content" onClick={handlePick}>
+          <div
+            className="upload-content"
+            onClick={handlePick}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
             <input
               type="file"
               accept="image/*, .png, .jpg, .gif, .web"
